a0b218999dc29ec465221e0043129dc96c33c5ea
Leo Kim <cloudkim2024@gmail.com>
2022-02-18 06:03:23 -0500

    Dinosaur , Cactus image added

--- main.js
+++ main.js
@@ -3,57 +3,102 @@ var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
 
+var dinoImage = new Image();
+dinoImage.src = 'dinosaur.png';
 var dino = {
     x : 10,   //start coordiate 
     y : 200, 
-    width : 50,
-    height : 50,
+    width : 30,
+    height : 30,
     draw() {
-        ctx.fillStyle = 'green';
-        ctx.fillRect(this.x, this.y, this.width, this.height);  //10,10 cooridate, size 100 x 100
+        //ctx.fillStyle = 'green';
+        //ctx.fillRect(this.x, this.y, this.width, this.height);  //10,10 cooridate, size 100 x 100
+        ctx.drawImage(dinoImage, this.x, this.y, 50, 50);
     }
 }
 //dino.draw();
 
 
+var cactusImage = new Image();
+cactusImage.src = 'cactus.png';
 class Obstacles{
     constructor(){
         this.x = 400;
         this.y = 200;
-        this.width = 50;
+        this.width = 20;
         this.height = 50;
     }
     draw() {
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.x, this.y, this.width, this.height);  //10,10 cooridate, size 100 x 100
+
+        //hit box
+        //ctx.fillStyle = 'red';
+        //ctx.fillRect(this.x, this.y, this.width, this.height);  //10,10 cooridate, size 100 x 100
+        ctx.drawImage(cactusImage, this.x, this.y, 40, 50);
     }
 }
-
 
 //cactus1.draw();
 
 var timer = 0;
 var obstaclesArray = []; 
-
+var jumpTimer = 0;
+var animation;
 
 function runOnFrame(){
-    requestAnimationFrame(runOnFrame)   //draw on page , HTML lib, 60 fps 
+    animation = requestAnimationFrame(runOnFrame)   //draw on page , HTML lib, 60 fps 
     timer++;
     ctx.clearRect(0,0 , canvas.width, canvas.height);
 
 
-    if(timer % 120 === 0) {
+    if(timer % 180 === 0) {
         var cactus = new Obstacles();
         obstaclesArray.push(cactus);
     }
 
-    obstaclesArray.forEach((a)=>{
+    obstaclesArray.forEach((a,i,o)=>{
         a.x--;
+        if (a.x < 0) {
+            o.splice(i,1)
+        }
+        //collision check 
+        collisionCheck(dino, a)
         a.draw();
     })
+    if (jumpSwitch == true) {
+        dino.y-=2;
+        jumpTimer++;
+    } else {
+        if (dino.y < 200) {
+            dino.y+=2;
+        }
+    }
+
+    if (jumpTimer > 60) {
+        jumpSwitch = false;
+        jumpTimer = 0;
+    }
+    
+    
     dino.draw();
     
 }
 
 runOnFrame();
+
+function collisionCheck(dino, obs) {
+
+    var x_diff = obs.x - (dino.x + dino.width )
+    var y_diff = obs.y - (dino.y + dino.height )
+    if (  x_diff < 0 && y_diff<0  ) {
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+        cancelAnimationFrame(animation)
+    }
+}
+
+var jumpSwitch = false; 
+document.addEventListener('keydown', function(e) {
+    if (e.code == 'Space') {
+        jumpSwitch = true;
+    }
+})
 
